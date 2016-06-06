da607a307af86d9825b582c4d56639d24bb485b0
Andrew62 <awoizesko@gmail.com>
2016-06-06 18:57:07 -0400

    using tensorflow data loading tools

--- cnn/vgg/data_loader.py
+++ cnn/vgg/data_loader.py
@@ -7,7 +7,7 @@ Github: https://github.com/andrew62
 
 import util
 import tensorflow as tf
-
+from config import workspace
 
 def input_pipline(fp, encoder, batch_size=256):
     """
@@ -19,23 +19,38 @@ def input_pipline(fp, encoder, batch_size=256):
         paths.append(path)
         labels.append(label)
     labels = encoder.encode(labels)
-    paths_tf = tf.convert_to_tensor(paths, dtype=tf.string)
-    labels_tf = tf.convert_to_tensor(labels, dtype=tf.float32)
-    producer = tf.train.slice_input_producer([paths_tf, labels_tf], shuffle=True)
-    image, label = img_prep(producer)
-    image_batch, label_batch = tf.train.batch([image, label], batch_size, capacity=(3*batch_size))
-
-    return image_batch, label_batch
+    paths_tf = tf.convert_to_tensor(paths, dtype=tf.string, name="image_paths")
+    labels_tf = tf.convert_to_tensor(labels, dtype=tf.float32, name="encoded_labels")
+    producer = tf.train.slice_input_producer([paths_tf, labels_tf], name="producer")
+    img_float, encoded_label = read_image(producer)
+    return tf.train.batch([img_float, encoded_label], batch_size=batch_size, capacity=(3*batch_size), name="batcher")
 
 
-def img_prep(queue):
-    label = queue[1]
-    path = queue[0]
-    file_contents = tf.read_file(path)
-    img = tf.image.decode_jpeg(file_contents, channels=3)
-    img_converted = tf.image.convert_image_dtype(img,  dtype=tf.float32)
-    mean = tf.reduce_mean(img_converted)
-    return_img = img_converted - mean
-    return_img.set_shape([256, 256, 3])
-    return return_img, label
+def read_image(queue):
+    paths, labels = queue
+    file_contents = tf.read_file(paths)
+    imgs = tf.image.decode_jpeg(file_contents, channels=3, name="input_jpg")
+    cast = tf.cast(imgs, dtype=tf.float32, name="float_img")
+    cast.set_shape(list(workspace.img_shape))
+    return cast, labels
 
+
+# if __name__ == "__main__":
+#     print "running"
+#     from cnn.config import workspace
+#     from cnn.encoder import  OneHot
+#     print "loading pkl"
+#     cls = util.pkl_load(workspace.class_pkl)
+#     print "creating encoder"
+#     encoder = OneHot(cls)
+#     print "creating graph"
+#     with tf.Graph().as_default():
+#         img, label = input_pipline(workspace.test_pkl, encoder, 10)
+#
+#         with tf.Session() as sess:
+#             sess.run(tf.initialize_all_variables())
+#             tf.train.start_queue_runners(sess=sess)
+#             for i in range(3):
+#                 content = sess.run([img, label])
+#                 print content[0].shape
+#     print 'done'
