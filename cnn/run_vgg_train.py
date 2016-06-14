35d98f702cf59f936aec42441c7fe1c4881b135e
Andrew62 <awoizesko@gmail.com>
2016-06-14 07:04:37 -0400

    fixed vgg connections and added all 19 layers

--- cnn/run_vgg_train.py
+++ cnn/run_vgg_train.py
@@ -11,7 +11,7 @@ from vgg.train import train_vgg
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", help="When True, runs a modified training script",
-                        type=bool, default=False)
+                        type=bool, default=True)
     args = parser.parse_args()
     train_vgg(debug=args.debug)
 
--- cnn/vgg/train.py
+++ cnn/vgg/train.py
@@ -14,9 +14,9 @@ import tensorflow as tf
 from data import DataSet
 from msg import send_mail
 from encoder import OneHot
-from datetime import datetime
 from config import workspace
-from cnn.wrapper import placeholder
+from datetime import datetime
+from wrapper import placeholder
 
 
 def train_vgg(debug=False):
@@ -24,41 +24,38 @@ def train_vgg(debug=False):
         print "DEBUG MODE"
         MESSAGE_EVERY = 1
         EMAILING = False
-        TRAIN_BATCH_SIZE = 5
+        TRAIN_BATCH_SIZE = 6
         SAVE_ITER = 1
-        TRAIN_ITER = 5
+        EPOCHS = 1
     else:
         MESSAGE_EVERY = 50
         EMAILING = True
-        TRAIN_BATCH_SIZE = 128
+        TRAIN_BATCH_SIZE = 256
         SAVE_ITER = 1000
-        TRAIN_ITER = 50000
+        EPOCHS = 60
 
-    MIDDLE_SHAPE=14*14*512
     EMAIL_EVERY = MESSAGE_EVERY * 20
     N_CLASSES = 252
     NUM_CORES = 4
 
     classes = util.pkl_load(workspace.class_pkl)
     encoder = OneHot(classes)
-    config = tf.ConfigProto(inter_op_parallelism_threads=NUM_CORES)
-    
     data = DataSet(workspace.train_pkl, workspace.test_pkl,
-               workspace.valid_pkl, workspace.class_pkl,
-               img_shape=(224, 224, 3))
+                   workspace.valid_pkl, workspace.class_pkl,
+                   img_shape=(224, 224, 3))
+
+    config = tf.ConfigProto(inter_op_parallelism_threads=NUM_CORES)
 
     graph = tf.Graph()
     with graph.as_default():
-        model = VGG(N_CLASSES, MIDDLE_SHAPE)
+        model = VGG(N_CLASSES)
         train_labels_placeholder = placeholder("train_labels", shape=None)
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(model.logits, train_labels_placeholder))
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+        optimizer = tf.train.AdagradOptimizer(learning_rate=0.001).minimize(loss)
 
-        sess = tf.Session(config=config, graph=graph)
+        sess = tf.Session(config=config)
         with sess.as_default():
             sess.run(tf.initialize_all_variables())
-            coord = tf.train.Coordinator()
-            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
             saver = tf.train.Saver()
             print "\n" + "*" * 50
             ckpt = tf.train.get_checkpoint_state(workspace.vgg_models)
@@ -72,25 +69,27 @@ def train_vgg(debug=False):
             print "Batch size: {0} images".format(TRAIN_BATCH_SIZE)
 
             performance_data = {}
+            epoch = 0
+            i = 0
             try:
-                for i in xrange(TRAIN_ITER):
+                while epoch <= EPOCHS:
                     performance_data[i] = {}
                     start = time.time()
                     # make the data object return raw labels
                     # make the encoder encode all labels separate from
                     # the data loader
-                    train_data, train_labels = data.train_batch(TRAIN_BATCH_SIZE)
+                    train_data, train_labels, epoch = data.train_batch(TRAIN_BATCH_SIZE)
                     train_lab_vec = encoder.encode(train_labels)
-    
-                    feed= {model.input_data: train_data,
-                             train_labels_placeholder: train_lab_vec,}
+
+                    feed = {model.input_data: train_data,
+                            train_labels_placeholder: train_lab_vec,}
                     _, sess_loss, predictions = sess.run([optimizer, loss, model.softmax],
                                                          feed_dict=feed)
-    
+
                     if ((i + 1) % MESSAGE_EVERY == 0) or (i == 0):
                         minibatch_accuracy = util.accuracy(predictions, train_lab_vec)
                         # valid_accuracy = util.accuracy(valid_prediction.eval(), valid_lab_vec)
-    
+
                         # collecting data for visualization later. Could prob use
                         # tensorboard
                         performance_data[i]['loss'] = sess_loss.mean()
@@ -110,21 +109,18 @@ def train_vgg(debug=False):
                         saver.save(sess, os.path.join(workspace.vgg_models, util.model_name(datetime.now())))
                         if EMAILING is True:
                             send_mail("Successful checkpoint", "Iteration {0}".format(i + 1))
-                    if debug is True and (i+1) == TRAIN_ITER:
-                        break
                     i += 1
                 msg = "\n" + "*" * 50
                 msg += "\n" + "*" * 50
                 # msg += "\nTest accuracy: {0:0.2%}".format(util.accuracy(test_prediction.eval(), test_lab_vec))
                 subj = "Training complete!"
                 print msg
-
             except Exception as e:
                 print e
                 subj = "DOGCATCHER STOPPED!"
                 msg = "Failed after {0} steps".format(i)
                 print msg
-            
+
             finally:
                 saver.save(sess, os.path.join(workspace.vgg_models, util.model_name(datetime.now())))
                 outg = os.path.join(workspace.vgg_models, "graph")
@@ -134,10 +130,7 @@ def train_vgg(debug=False):
 
                 util.write_csv(performance_data, os.path.join(workspace.vgg_models, 'performance.csv'))
                 util.pkl_dump(encoder, os.path.join(workspace.vgg_models, "encoder.pkl"))
-                coord.request_stop()
-                coord.join(threads)
                 if EMAILING is True:
                     send_mail(subj, msg)
-
 
 
