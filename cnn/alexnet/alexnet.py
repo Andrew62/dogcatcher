6be2ce6d86e529f1f0aa8c47974fc8ea810c629d
Andrew62 <awoizesko@gmail.com>
2016-06-19 17:49:07 -0400

    changed max pooling window from [1, 2, 2, 1] to [1, 3, 3, 1]

--- cnn/alexnet/alexnet.py
+++ cnn/alexnet/alexnet.py
@@ -5,7 +5,7 @@ Tensorflow implementation of AlexNet
 import tensorflow as tf
 
 class AlxNet(object):
-    def __init__(self, n_classes, keep_prob=0.5, train=False):
+    def __init__(self, n_classes, keep_prob=0.5, train=False, lrn=False):
         """
         The middle shape in AlexNet is designed to be 4096, however,
         we are using larger images so the fully connected layers
@@ -14,6 +14,7 @@ class AlxNet(object):
         self.input_data = tf.placeholder(tf.float32, name="input_data")#, shape=(256, 224, 224, 3))
         self.keep_prob = tf.constant(keep_prob, name="Dropout", dtype=tf.float32)
         self.train = train
+        self.lrn = lrn
 
         self.n_classes = n_classes
 
@@ -21,12 +22,13 @@ class AlxNet(object):
             with tf.variable_scope('conv1'):
                 self.weights1 = tf.get_variable('weights', [11, 11, 3, 48],
                                                 initializer=tf.random_normal_initializer(stddev=1e-2))
-                self.bias1 = tf.get_variable('bias', [48], initializer=tf.constant_initializer(0.0))
+                self.bias1 = tf.get_variable('bias', [48], initializer=tf.constant_initializer(0.1))
                 self.convolve1 = tf.nn.conv2d(self.input_data, self.weights1, [1, 4, 4, 1], 'SAME')
                 self.conv1 = tf.nn.relu(self.convolve1 + self.bias1)
-            self.norm1 = tf.nn.local_response_normalization(self.conv1, depth_radius=2, alpha=2e-5,
-                                                            beta=0.75, bias=1.0)
-            self.pool1 = tf.nn.max_pool(self.norm1, [1, 2, 2, 1], [1, 2, 2, 1], padding="SAME")
+            if self.lrn is True:
+                self.conv1 = tf.nn.local_response_normalization(self.conv1, depth_radius=2, alpha=2e-5,
+                                                                beta=0.75, bias=1.0)
+            self.pool1 = tf.nn.max_pool(self.conv1, [1, 3, 3, 1], [1, 2, 2, 1], padding="SAME")
 
 
         with tf.variable_scope("pool2"):
@@ -36,15 +38,16 @@ class AlxNet(object):
                 self.bias2 = tf.get_variable('bias', [128], initializer=tf.constant_initializer(1.0))
                 self.convolve2 = tf.nn.conv2d(self.pool1, self.weights2, [1, 1, 1, 1], 'SAME')
                 self.conv2 = tf.nn.relu(self.convolve2 + self.bias2)
-            self.norm2 = tf.nn.local_response_normalization(self.conv2, depth_radius=2, alpha=2e-5,
-                                                            beta=0.75, bias=1.0)
-            self.pool2 = tf.nn.max_pool(self.norm2, [1, 2, 2, 1], [1, 2, 2, 1], padding="SAME")
+            if self.lrn is True:
+                self.conv2 = tf.nn.local_response_normalization(self.conv2, depth_radius=2, alpha=2e-5,
+                                                                beta=0.75, bias=1.0)
+            self.pool2 = tf.nn.max_pool(self.conv2, [1, 3, 3, 1], [1, 2, 2, 1], padding="SAME")
 
         with tf.variable_scope("pool3"):
             with tf.variable_scope('conv3'):
                 self.weights3 = tf.get_variable('weights', [3, 3, 128, 192],
                                                 initializer=tf.random_normal_initializer(stddev=1e-2))
-                self.bias3 = tf.get_variable('bias', [192], initializer=tf.constant_initializer(0.0))
+                self.bias3 = tf.get_variable('bias', [192], initializer=tf.constant_initializer(0.1))
                 self.convolve3 = tf.nn.conv2d(self.pool2, self.weights3, [1, 1, 1, 1], 'SAME')
                 self.conv3 = tf.nn.relu(self.convolve3 + self.bias3)
 
@@ -62,11 +65,8 @@ class AlxNet(object):
                 self.convolve5 = tf.nn.conv2d(self.conv4, self.weights5, [1, 1, 1, 1], 'SAME')
                 self.conv5 = tf.nn.relu(self.convolve5 + self.bias5)
 
-            self.pool5 = tf.nn.max_pool(self.conv5, [1, 2, 2, 1], [1, 2, 2, 1], padding="SAME")
-            # pool_5_shape = self.pool5.get_shape().as_list()[1:]
-            # middle_shape = 1
-            # for x in pool_5_shape:
-            #     middle_shape *= x
+            self.pool5 = tf.nn.max_pool(self.conv5, [1, 3, 3, 1], [1, 2, 2, 1], padding="SAME")
+
             middle_shape = 6272
 
             self.reshape5 = tf.reshape(self.pool5, [-1, middle_shape])
