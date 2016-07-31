bd3a6318193ea5f1898d682ad1f65d1b36da3c45
Andrew62 <awoizesko@gmail.com>
2016-07-30 21:35:01 -0400

    working on making code more concise

---
+++ cnn/networks/layers.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+Author: Andrew
+Github: https://github.com/andrew62
+"""
+
+import tensorflow as tf
+from operator import mul
+
+
+def conv2d(inputs, n_out, kernel_height, kernel_width, **kwargs):
+    """
+    :param inputs: input layer
+    :param kernel_height: conv filter kernel width
+    :param kernel_width: conv filter kernel height
+    :param channles_out: conv depth
+    :param name: layer name
+    :param conv_height: verticle steps for conv filter
+    :param conv_width: horizontal steps for conv filter
+    :param relu: whether or not to apply Relu
+    :param bias: whether or not to apply bias
+    :param padding: padding to use for conv. Either "SAME" or "VALID"
+    :return: conv layer
+    """
+    name = kwargs.pop('name','conv')
+    conv_height = kwargs.pop('conv_height', 1)
+    conv_width = kwargs.pop('conv_width', 1)
+    relu = kwargs.pop('relu', True)
+    bias = kwargs.pop('bias', True)
+    padding = kwargs.pop("padding", "SAME")
+
+    channles_in = inputs.get_shape().as_list()[-1]
+
+    with tf.variable_scope(name) as scope:
+        weights = tf.get_variable('weights', [kernel_height, kernel_width, channles_in, n_out],
+                                  initializer=tf.random_normal_initializer(stddev=1e-2))
+        convolve = tf.nn.conv2d(inputs, weights, [1, conv_height, conv_width, 1],
+                                padding=padding)
+        if bias is True:
+            bias_layer = tf.get_variable("biases", [n_out], initializer=tf.constant_initializer(0.0))
+            convolve = tf.nn.bias_add(convolve, bias_layer)
+
+        if relu is True:
+            convolve = tf.nn.relu(convolve)
+
+        return convolve
+
+
+def affine(inputs, n_out, **kwargs):
+    """
+
+    :param inputs: tensor
+    :param n_out: shape of outputs
+    :param name: layer name
+    :param bias: bool. Use bias or not
+    :param relu: bool. Use relu or not
+    :return: affine tensor
+    """
+    name = kwargs.pop("name", "affine")
+    bias = kwargs.pop("bias", True)
+    relu = kwargs.pop("relu", True)
+
+    input_shape = inputs.get_shape().as_list()
+    if len(input_shape) == 4:
+        n_in = reduce(mul, input_shape[1:], 1)
+        inputs = tf.reshape(inputs, shape=[-1, n_in])
+    else:
+        n_in = input_shape[-1]
+
+    with tf.variable_scope(name) as scope:
+        weights = tf.get_variable('weights', [n_in, n_out], initializer=tf.random_normal_initializer(stddev=1e-2))
+        fc = tf.matmul(inputs, weights)
+
+        if bias is True:
+            bias_layer = tf.get_variable("biases", [n_out], initializer=tf.constant_initializer(0.0))
+            fc = tf.nn.bias_add(fc, bias_layer)
+
+        if relu is True:
+            fc = tf.nn.relu(fc)
+
+        return fc
--- cnn/networks/vgg16_D.py
+++ cnn/networks/vgg16_D.py
@@ -1,142 +1,51 @@
-
 """
 A tensorflow implementation of VGG19. Structure was pulled
 from the paper.
 """
 
+import layers
 import tensorflow as tf
 
+
 class VGG16_D(object):
+    def __init__(self, n_classes=1000, train=False):
 
-    def __init__(self, n_classes=1000, keep_prob=0.5, train=False):
-        self.middle_shape = 25088
+        self.input_data = tf.placeholder(dtype=tf.float32, name='input_data', shape=[32, 224, 224, 3])
 
-        self.keep_prob = tf.constant(keep_prob, name="Dropout", dtype=tf.float32)
-
-        self.input_data = tf.placeholder(dtype=tf.float32, name='input_data')
-        
-        mean = tf.constant([125.974950491, 121.990847064, 102.991749558], 
+        mean = tf.constant([125.974950491, 121.990847064, 102.991749558],
                            dtype=tf.float32, shape=[1, 1, 1, 3], name='img_mean')
-        
+
         self.mean_subtract = self.input_data - mean
-        
-        with tf.variable_scope("conv1_1"):
-            self.weights1 = tf.get_variable('weights',[3, 3, 3, 64], initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias1 = tf.get_variable("biases", [64], initializer=tf.constant_initializer(0.0))
-            self.convolve1 = tf.nn.conv2d(self.mean_subtract, self.weights1, [1, 1, 1, 1],
-                                          padding="SAME")
-            self.conv1 = tf.nn.relu(tf.nn.bias_add(self.convolve1, self.bias1))
 
-        with tf.variable_scope("conv1_2"):
-            self.weights2 = tf.get_variable('weights', [3, 3, 64, 64],
-                                            initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias2 = tf.get_variable("biases", [64], initializer=tf.constant_initializer(0.0))
-            self.convolve2 = tf.nn.conv2d(self.conv1, self.weights2, [1, 1, 1, 1], padding="SAME")
-            self.conv2 = tf.nn.relu(tf.nn.bias_add(self.convolve2, self.bias2))
+        self.layers = []
 
-        self.pool1 = tf.nn.max_pool(self.conv2, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME', name='pool1')
+        inputs = self.input_data
+        for i, n_out in enumerate([64, 128, 256, 512, 512]):
+            conv = layers.conv2d(inputs, n_out, 3, 3, name="conv_{}_1".format(i))
+            self.layers.append(conv)
+            conv = layers.conv2d(conv, n_out, 3, 3, name="conv_{}_2".format(i))
+            self.layers.append(conv)
 
-        with tf.variable_scope("conv2_1"):
-            self.weights3 = tf.get_variable('weights', [3, 3, 64, 128],
-                                            initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias3 = tf.get_variable("biases", [128], initializer=tf.constant_initializer(0.0))
-            self.convolve3 = tf.nn.conv2d(self.pool1, self.weights3, [1, 1, 1, 1], padding="SAME")
-            self.conv3 = tf.nn.relu(tf.nn.bias_add(self.convolve3, self.bias3))
-        with tf.variable_scope("conv2_2"):
-            self.weights4 = tf.get_variable('weights', [3, 3, 128, 128],
-                                            initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias4 = tf.get_variable("biases", [128], initializer=tf.constant_initializer(0.0))
-            self.convolve4 = tf.nn.conv2d(self.conv3, self.weights4, [1, 1, 1, 1], padding="SAME")
-            self.conv4 = tf.nn.relu(tf.nn.bias_add(self.convolve4, self.bias4))
-        self.pool2 = tf.nn.max_pool(self.conv4, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME', name='pool2')
+            if n_out > 128:
+                conv = layers.conv2d(conv, n_out, 3, 3, name="conv_{}_3".format(i))
+                self.layers.append(conv)
 
-        with tf.variable_scope("conv3_1"):
-            self.weights5 = tf.get_variable('weights', [3, 3, 128, 256],
-                                            initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias5 = tf.get_variable("biases", [256], initializer=tf.constant_initializer(0.0))
-            self.convolve5 = tf.nn.conv2d(self.pool2, self.weights5, [1, 1, 1, 1], padding="SAME")
-            self.conv5 = tf.nn.relu(tf.nn.bias_add(self.convolve5, self.bias5))
-        with tf.variable_scope("conv3_2"):
-            self.weights6 = tf.get_variable('weights', [3, 3, 256, 256],
-                                            initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias6 = tf.get_variable("biases", [256], initializer=tf.constant_initializer(0.0))
-            self.convolve6 = tf.nn.conv2d(self.conv5, self.weights6, [1, 1, 1, 1], padding="SAME")
-            self.conv6 = tf.nn.relu(tf.nn.bias_add(self.convolve6, self.bias6))
-        with tf.variable_scope("conv3_3"):
-            self.weights7 = tf.get_variable('weights', [3, 3, 256, 256],
-                                            initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias7 = tf.get_variable("biases", [256], initializer=tf.constant_initializer(0.0))
-            self.convolve7 = tf.nn.conv2d(self.conv6, self.weights7, [1, 1, 1, 1], padding="SAME")
-            self.conv7 = tf.nn.relu(tf.nn.bias_add(self.convolve7, self.bias7))
-        self.pool3 = tf.nn.max_pool(self.conv7, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME', name='pool3')
+            inputs = tf.nn.max_pool(conv, [1, 2, 2, 1], [1, 2, 2, 1], padding="SAME")
+            self.layers.append(inputs)
 
-        with tf.variable_scope("conv4_1"):
-            self.weights8 = tf.get_variable('weights', [3, 3, 256, 512],
-                                            initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias8 = tf.get_variable("biases", [512], initializer=tf.constant_initializer(0.0))
-            self.convolve8 = tf.nn.conv2d(self.pool3, self.weights8, [1, 1, 1, 1], padding="SAME")
-            self.conv8 = tf.nn.relu(tf.nn.bias_add(self.convolve8, self.bias8))
-        with tf.variable_scope("conv4_2"):
-            self.weights9 = tf.get_variable('weights', [3, 3, 512, 512],
-                                            initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias9 = tf.get_variable("biases", [512], initializer=tf.constant_initializer(0.0))
-            self.convolve9 = tf.nn.conv2d(self.conv8, self.weights9, [1, 1, 1, 1], padding="SAME")
-            self.conv9 = tf.nn.relu(tf.nn.bias_add(self.convolve9, self.bias9))
-        with tf.variable_scope("conv4_3"):
-            self.weights10 = tf.get_variable('weights', [3, 3, 512, 512],
-                                             initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias10 = tf.get_variable("biases", [512], initializer=tf.constant_initializer(0.0))
-            self.convolve10 = tf.nn.conv2d(self.conv9, self.weights10, [1, 1, 1, 1], padding="SAME")
-            self.conv10 = tf.nn.relu(tf.nn.bias_add(self.convolve10, self.bias10))
-        self.pool4 = tf.nn.max_pool(self.conv10, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME', name="pool4")
-
-        with tf.variable_scope("conv5_1"):
-            self.weights11 = tf.get_variable('weights', [3, 3, 512, 512],
-                                             initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias11 = tf.get_variable("biases", [512], initializer=tf.constant_initializer(0.0))
-            self.convolve11 = tf.nn.conv2d(self.pool4, self.weights11, [1, 1, 1, 1], padding="SAME")
-            self.conv11 = tf.nn.relu(tf.nn.bias_add(self.convolve11, self.bias11))
-
-        with tf.variable_scope("conv5_2"):
-            self.weights12 = tf.get_variable('weights', [3, 3, 512, 512],
-                                             initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias12 = tf.get_variable("biases", [512], initializer=tf.constant_initializer(0.0))
-            self.convolve12 = tf.nn.conv2d(self.conv11, self.weights12, [1, 1, 1, 1], padding="SAME")
-            self.conv12 = tf.nn.relu(tf.nn.bias_add(self.convolve12, self.bias12))
-
-        with tf.variable_scope("conv5_3"):
-            self.weights13 = tf.get_variable('weights', [3, 3, 512, 512],
-                                             initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias13 = tf.get_variable("biases", [512], initializer=tf.constant_initializer(0.0))
-            self.convolve13 = tf.nn.conv2d(self.conv12, self.weights13, [1, 1, 1, 1], padding="SAME")
-            self.conv13 = tf.nn.relu(tf.nn.bias_add(self.convolve13, self.bias13))
-        self.pool5 = tf.nn.max_pool(self.conv13, [1, 2, 2, 1], [1, 2, 2, 1], "SAME", name="pool5")
-        self.pool5_reshape = tf.reshape(self.pool5, [-1, self.middle_shape], 'pool5_reshape')
-
-        with tf.variable_scope("fc6"):
-            self.weights14 = tf.get_variable('weights', [self.middle_shape, 4096],
-                                             initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias14 = tf.get_variable("biases", [4096], initializer=tf.constant_initializer(0.0))
-            self.fc6 = tf.nn.relu(tf.nn.bias_add(tf.matmul(self.pool5_reshape, self.weights14), self.bias14))
+        for i in range(2):
+            inputs = layers.affine(inputs, 4096, name="fc_{}".format(i + 1))
             if train is True:
-                self.fc6 = tf.nn.dropout(self.fc6, self.keep_prob)
+                inputs = tf.nn.dropout(inputs, keep_prob=0.5)
+            self.layers.append(inputs)
 
-        with tf.variable_scope("fc7"):
-            self.weights15 = tf.get_variable('weights', [4096, 4096],
-                                             initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias15 = tf.get_variable("biases", [4096], initializer=tf.constant_initializer(0.0))
-            self.fc7 = tf.nn.relu(tf.nn.bias_add(tf.matmul(self.fc6, self.weights15), self.bias15))
-            if train is True:
-                self.fc7 = tf.nn.dropout(self.fc7, self.keep_prob)
-
-        with tf.variable_scope("fc8"):
-            self.weights16 = tf.get_variable('weights', [4096, n_classes],
-                                             initializer=tf.random_normal_initializer(stddev=1e-2))
-            self.bias16 = tf.get_variable("biases", [n_classes], initializer=tf.constant_initializer(0.0))
-            self.logits = tf.nn.bias_add(tf.matmul(self.fc7, self.weights16), self.bias16)
+        self.logits = layers.affine(inputs, n_classes, relu=False)
 
         self.softmax = tf.nn.softmax(self.logits, 'softmax')
 
 
 if __name__ == "__main__":
-    vgg = VGG16(252)
+    vgg = VGG16_D(252)
+
+    for l in vgg.layers:
+        print l.name
