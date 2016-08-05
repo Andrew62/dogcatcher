d6c0f8ca65ebf47e5a5b64b640807e1f4164fdb1
Andrew62 <awoizesko@gmail.com>
2016-08-05 18:45:20 -0400

    layer sizes

--- cnn/networks/alexnet.py
+++ cnn/networks/alexnet.py
@@ -31,46 +31,46 @@ class AlxNet(object):
 
         with tf.variable_scope("pool1"):
             with tf.variable_scope('conv1'):
-                self.weights1 = tf.Variable(tf.truncated_normal([11, 11, 3, 48], dtype=tf.float32, stddev=1e-2))
-                self.bias1 = tf.Variable(tf.constant(0.01, shape=[48], dtype=tf.float32))
-                self.conv1 = tf.nn.conv2d(self.batch_norm, self.weights1, [1, 4, 4, 1], 'SAME')
+                self.weights1 = tf.Variable(tf.truncated_normal([11, 11, 3, 96], dtype=tf.float32, stddev=1e-2))
+                self.bias1 = tf.Variable(tf.constant(0.01, shape=[96], dtype=tf.float32))
+                self.conv1 = tf.nn.conv2d(self.batch_norm, self.weights1, [1, 4, 4, 1], 'VALID')
                 self.hidden1 = tf.nn.relu(self.conv1 + self.bias1)
                 self.response_norm1 = tf.nn.local_response_normalization(self.hidden1, depth_radius=5, alpha=1e-3,
                                                                     beta=0.75, bias=2.0)
-            self.pool1 = tf.nn.max_pool(self.response_norm1, [1, 2, 2, 1], [1, 1, 1, 1], padding="VALID")
+            self.pool1 = tf.nn.max_pool(self.response_norm1, [1, 3, 3, 1], [1, 2, 2, 1], padding="VALID")
             variable_summaries(self.pool1, 'pool1')
 
         with tf.variable_scope("pool2"):
             with tf.variable_scope('conv2'):
-                self.weights2 = tf.Variable(tf.truncated_normal([5, 5, 48, 128], stddev=1e-2, dtype=tf.float32))
-                self.bias2 = tf.Variable(tf.constant(0.01, dtype=tf.float32, shape=[128]))
+                self.weights2 = tf.Variable(tf.truncated_normal([5, 5, 96, 256], stddev=1e-2, dtype=tf.float32))
+                self.bias2 = tf.Variable(tf.constant(0.1, dtype=tf.float32, shape=[256]))
                 self.conv2 = tf.nn.conv2d(self.pool1, self.weights2, [1, 1, 1, 1], 'SAME')
                 self.hidden2 = tf.nn.relu(self.conv2 + self.bias2)
                 self.response_norm2 = tf.nn.local_response_normalization(self.hidden2, depth_radius=5, alpha=1e-3,
                                                                     beta=0.75, bias=2.0)
-            self.pool2 = tf.nn.max_pool(self.response_norm2, [1, 3, 3, 1], [1, 2, 2, 1], padding="VALID")
+            self.pool2 = tf.nn.max_pool(self.response_norm2, [1, 3, 3, 1], [1, 2, 2, 1], padding="SAME")
             variable_summaries(self.pool2, 'pool2')
 
         with tf.variable_scope("pool3"):
             with tf.variable_scope('conv3'):
-                self.weights3 = tf.Variable(tf.truncated_normal([3, 3, 128, 192], dtype=tf.float32, stddev=1e-2))
-                self.bias3 = tf.Variable(tf.constant(0.01, shape=[192], dtype=tf.float32))
-                self.conv3 = tf.nn.conv2d(self.pool2, self.weights3, [1, 2, 2, 1], 'VALID')
+                self.weights3 = tf.Variable(tf.truncated_normal([3, 3, 256, 384], dtype=tf.float32, stddev=1e-2))
+                self.bias3 = tf.Variable(tf.constant(0.01, shape=[384], dtype=tf.float32))
+                self.conv3 = tf.nn.conv2d(self.pool2, self.weights3, [1, 1, 1, 1], 'SAME')
                 self.hidden3 = tf.nn.relu(self.conv3 + self.bias3)
 
             with tf.variable_scope("conv4"):
-                self.weights4 = tf.Variable(tf.truncated_normal([3, 3, 192, 192], stddev=1e-2, dtype=tf.float32))
-                self.bias4 = tf.Variable(tf.constant(0.01, shape=[192], dtype=tf.float32))
+                self.weights4 = tf.Variable(tf.truncated_normal([3, 3, 384, 384], stddev=1e-2, dtype=tf.float32))
+                self.bias4 = tf.Variable(tf.constant(0.1, shape=[384], dtype=tf.float32))
                 self.conv4 = tf.nn.conv2d(self.hidden3, self.weights4, [1, 1, 1, 1], 'SAME')
                 self.hidden4 = tf.nn.relu(self.conv4 + self.bias4)
 
             with tf.variable_scope("conv5"):
-                self.weights5 = tf.Variable(tf.truncated_normal([3, 3, 192, 128], stddev=1e-2, dtype=tf.float32))
-                self.bias5 = tf.Variable(tf.constant(0.01, dtype=tf.float32, shape=[128]))
+                self.weights5 = tf.Variable(tf.truncated_normal([3, 3, 384, 256], stddev=1e-2, dtype=tf.float32))
+                self.bias5 = tf.Variable(tf.constant(0.01, dtype=tf.float32, shape=[256]))
                 self.conv5 = tf.nn.conv2d(self.hidden4, self.weights5, [1, 1, 1, 1], 'SAME')
                 self.hidden5 = tf.nn.relu(self.conv5 + self.bias5)
 
-            self.pool5 = tf.nn.max_pool(self.hidden5, [1, 2, 2, 1], [1, 1, 1, 1], padding="SAME")
+            self.pool5 = tf.nn.max_pool(self.hidden5, [1, 3, 3, 1], [1, 2, 2, 1], padding="VALID")
             variable_summaries(self.pool5, 'pool5')
             middle_shape = reduce(mul, self.pool5.get_shape().as_list()[1:], 1)
             self.reshape5 = tf.reshape(self.pool5, [-1, middle_shape])
