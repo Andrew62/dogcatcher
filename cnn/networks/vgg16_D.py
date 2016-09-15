779f3c923fa3815214cbdfc5fbae62eae8c89533
Andrew62 <awoizesko@gmail.com>
2016-09-15 09:22:33 -0700

    refactored to collect layers in a dictionary

--- cnn/networks/vgg16_D.py
+++ cnn/networks/vgg16_D.py
@@ -10,48 +10,41 @@ import tensorflow as tf
 class VGG16_D(object):
     def __init__(self, n_classes=1000, train=False):
 
+        self.layers = {}
+
         self.input_data = tf.placeholder(dtype=tf.float32, name='input_data', shape=[None, 224, 224, 3])
 
-        name = 'batch_norm'
-        self.layer_names.append(name)
-        with tf.variable_scope(name):
-            mean, var = tf.nn.moments(self.input_data, axes=[0, 1, 2])
-            self.batch_norm = tf.nn.batch_normalization(self.input_data, mean, var, offset=1, scale=1,
-                                                        variance_epsilon=1e-6)
-        tf.image_summary(name, self.batch_norm)
+        batch_norm = self.apply_batch_norm(self.input_data)
+        self.layers['group1'] = self.pooling_group(batch_norm, 64, 'group1')
+        self.layers['group2'] = self.pooling_group(self.layers['group1'], 128, 'group2')
+        self.layers['group3'] = self.pooling_group(self.layers['group2'], 128, 'group3', 3)
+        self.layers['group4'] = self.pooling_group(self.layers['group3'], 256, 'group4', 3)
+        self.layers['group5'] = self.pooling_group(self.layers['group4'], 512, 'group5', 3)
+        self.layers['group6'] = self.pooling_group(self.layers['group5'], 512, 'group6', 3)
+        self.layers['fc1'] = layers.affine(self.layers['group6'], 4096, name='fc1')
+        self.layers['fc2'] = layers.affine(self.layers['fc1'], 4096, name='fc1')
+        self.logits = layers.affine(self.layers['fc2'], n_classes, name='logits', relu=False)
+        self.softmax = tf.nn.softmax(self.logits, name='softmax')
 
-        if train is True:
-            tf.image_summary("batch_norm", self.batch_norm)
+    def pooling_group(self, inputs, n_out, name, n_conv=2):
+        with tf.variable_op_scope([inputs], name, 'PoolingGroup') as sc:
+            layer_collection = {}
+            current_input = inputs
+            for i in range(n_conv):
+                layer_collection['conv{0}'.format(i + 1)] = layers.conv2d(current_input, n_out, 3, 3, scope=sc)
+                current_input = layer_collection['conv{0}'.format(i + 1)]
+            layer_collection['pool'] = tf.nn.max_pool(current_input, [1, 2, 2, 1], [1, 2, 2, 1], padding="VALID")
+        layers.variable_summaries(layer_collection['pool'], name)
+        self.layers[name] = layer_collection
+        return layer_collection['pool']
 
-        self.layers = []
+    def apply_batch_norm(self, inputs):
+        mean, var = tf.nn.moments(inputs, axes=[0, 1, 2])
+        batch_norm = tf.nn.batch_normalization(inputs, mean, var, offset=1, scale=1,
+                                               variance_epsilon=1e-6)
+        return batch_norm
 
-        inputs = self.batch_norm
-        for i, n_out in enumerate([64, 128, 256, 512, 512]):
-            with tf.variable_scope("pool_{}".format(i + 1)):
-                conv = layers.conv2d(inputs, n_out, 3, 3, name="conv_{}_1".format(i))
-                self.layers.append(conv)
-                conv = layers.conv2d(conv, n_out, 3, 3, name="conv_{}_2".format(i))
-                self.layers.append(conv)
 
-                if n_out > 128:
-                    conv = layers.conv2d(conv, n_out, 3, 3, name="conv_{}_3".format(i))
-                    self.layers.append(conv)
-
-                inputs = tf.nn.max_pool(conv, [1, 2, 2, 1], [1, 2, 2, 1], padding="VALID")
-                self.layers.append(inputs)
-
-                if train is True:
-                    layers.variable_summaries(inputs, "pool_{}".format(i + 1))
-
-        for i in range(2):
-            inputs = layers.affine(inputs, 4096, name="fc_{}".format(i + 1))
-            if train is True:
-                inputs = tf.nn.dropout(inputs, keep_prob=0.5)
-            self.layers.append(inputs)
-
-        self.logits = layers.affine(inputs, n_classes, relu=False)
-
-        self.softmax = tf.nn.softmax(self.logits, 'softmax')
 
 
 if __name__ == "__main__":
